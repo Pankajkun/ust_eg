38c1b10cddf903205240b42b6c6dfa4e454bbcde
Pankaj <pnkjojha4@gmail.com>
2024-06-17 16:55:05 +0530

    Last change probably

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 def calcu(a, b):
-    return f' product is {a*b}, sum is {a+b}, diff is {a-b}, division is {a/b}
+    return (f' product is {a*b}, sum is {a+b}, diff is {a-b}, division is {a/b}, {a} is {a/b * 100}% of {b}')
 
 if __name__  == "__main__":
     print(calcu(10, 2))
